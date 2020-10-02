13233169e19703543308a60210b7ab9228107fcf
Hiroki Sakai <sakhirok@fsi.co.jp>
2020-10-02 09:44:26 +0900

    tabbar用に画面作成

--- MeshiMatching/MeshiMatching/ViewController.swift
+++ MeshiMatching/MeshiMatching/ViewController.swift
@@ -15,6 +15,12 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        self.enterButton.layer.shadowColor = UIColor.yellow.cgColor
+        self.enterButton.layer.cornerRadius = 8 //角丸
+        self.enterButton.layer.shadowOpacity = 1 // 影の透明度。値が大きくなればなるほど透明になる
+        self.enterButton.layer.shadowRadius = 4 // 影のぼかし。値が大きくなればなるほどフワフワする
+        self.enterButton.layer.shadowOffset = CGSize(width:0, height: 0) // 影の方向。widthは正の値が右に影、負の値が左、heightは正の値が下、負の値が上
+        
     }
 
     @IBAction func enter(_ sender: Any) {
